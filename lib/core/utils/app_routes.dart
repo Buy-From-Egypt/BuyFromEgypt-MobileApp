28e9e21dd00d0826bfabec5d3c96e90ec7877af1
unknown <shahdrhoma990@gmaile.com>
2025-06-13 19:34:35 +0300

    your message

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -2,7 +2,6 @@ class AppRoutes {
   static const String splash = '/';
   static const String auth = '/authView';
   static const String onboarding = '/onboardingScreen';
-<<<<<<< HEAD
   static const String forgetPassword = '/forgetPasswordView';
   static const String otpForgetPassword = '/otpForgetPassword';
   static const String updatePassword = '/updatePasswordView';
@@ -12,14 +11,5 @@ class AppRoutes {
   static const String search = '/searchView';
   static const String pending = '/pendingView';
   static const String pendingMoreInfo = '/pendingMoreInfo';
-=======
-  static const String forgetPassword = 'forgetPasswordView';
-  static const String otpForgetPassword = 'otpForgetPassword';
-  static const String updatePassword = 'updatePasswordView';
-  static const String successfully = 'successfullyView';
-  static const String preference = 'preferenceView';
-  static const String home = 'homeView';
-  static const String search = 'searchView';
-  static const String comment = 'commentView';
->>>>>>> shahd
+  static const String comment = '/commentView';
 }
